b9a483655f6f2e77efa968efab1bd666668ae8a9
Grzegorz <grzegorzmickowski@MacBook-Air-Karina.local>
2017-01-17 10:28:46 +0100

    clients array

--- server.py
+++ server.py
@@ -3,10 +3,16 @@ from twisted.internet import reactor
  
 class IphoneChat(Protocol):
     def connectionMade(self):
+    	self.factory.clients.append(self)
         print "a client connected"
+        print "clients are ", self.factory.clients
+
+    def connectionLost(self, reason):
+    	self.factory.clients.remove(self)
  
 factory = Factory()
 factory.protocol = IphoneChat
+factory.clients[]
 reactor.listenTCP(80, factory)
 print "Iphone Chat server started"
 reactor.run()
